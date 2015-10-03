333cba90f77cf528e7ea9ab5b2b41e4ef627d4c0
Cristian Poll <cpoll24@gmail.com>
2015-10-03 10:04:58 -0400

    generate yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanTutorialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanTutorialApp
+ */
+angular.module('yeomanTutorialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
