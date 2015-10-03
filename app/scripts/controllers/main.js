57e98ca1ecc0c2bc37a07d91815526819243f369
Cristian Poll <cpoll24@gmail.com>
2015-10-03 14:50:10 -0400

    add remove todo item functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,4 +17,8 @@ angular.module('yeomanTutorialApp')
 	  $scope.todos.push($scope.todo);
 	  $scope.todo = '';
 	};
+	
+	$scope.removeTodo = function (index) {
+	  $scope.todos.splice(index, 1);
+	};
   });
